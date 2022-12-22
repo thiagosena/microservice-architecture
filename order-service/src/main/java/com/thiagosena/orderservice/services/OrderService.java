d34083c4be1cee0b15d3dd8b3708dfc648aed4c5
Thiago Sena <thiagosena.dev@gmail.com>
2022-12-22 12:59:33 -0300

    feat(discovery): add eureka server and client

--- order-service/src/main/java/com/thiagosena/orderservice/services/OrderService.java
+++ order-service/src/main/java/com/thiagosena/orderservice/services/OrderService.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public OrderResponse placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -39,8 +39,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode).toList();
 
         // Call inventory service and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventories",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventories",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
