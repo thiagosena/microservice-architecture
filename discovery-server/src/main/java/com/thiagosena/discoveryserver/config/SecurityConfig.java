ac14a8a5d2130d3b88cfa60d67b8ab26c9982ce6
Thiago Sena <thiagosena.dev@gmail.com>
2022-12-28 19:01:39 -0300

    feat(security): add keycloak on api gateway and security on discovery service

---
+++ discovery-server/src/main/java/com/thiagosena/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.thiagosena.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
+
+    @Bean
+    //Authentication config - Login
+    public UserDetailsService getAuthenticationManager() {
+        UserDetails admin = User.builder()
+                .username(username)
+                .password(password)
+                .passwordEncoder(s -> passwordEncoder().encode(s))
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    // Authorization config
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
